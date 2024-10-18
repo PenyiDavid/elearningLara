28ce0703325a29bfa3314b82e586b403a7169a60
Penyaskó Dávid <penyaskodavid@gmail.com>
2024-10-18 11:42:24 +0200

    Subject

--- elearningOra/app/Http/Controllers/SubjectController.php
+++ elearningOra/app/Http/Controllers/SubjectController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
-    //
+    public function index(){
+        return view('subjects.create');
+    }
+
+    public function store(Request $request){
+        $request->validate(
+            ['subject_name' => 'required|string'],
+        );
+
+        Subject::create($request->all());
+        return redirect()->back()->with('success', 'Subject stored.');
+    }
+
 }
--- elearningOra/routes/web.php
+++ elearningOra/routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/new-subject', [SubjectController::class, 'index'])->name('subject.index');
+Route::post('/new-subject', [SubjectController::class, 'store'])->name('subject.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
