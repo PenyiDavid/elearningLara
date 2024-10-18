a9861341072a38e9a3ad2b212c43dfcf610f8429
Penyaskó Dávid <penyaskodavid@gmail.com>
2024-10-18 10:29:43 +0200

    Models: answer, score

--- elearningOra/app/Models/Answer.php
+++ elearningOra/app/Models/Answer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['answer_text', 'question_id', 'is_correct'];
+
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
 }
--- elearningOra/app/Models/Scores.php
+++ elearningOra/app/Models/Scores.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Scores extends Model
 {
-    //
+    use HasFactory, softDeletes;
+
+    protected $fillable = ['subject_id', 'score', 'email'];
+
+    public function subject(){
+        return $this->belongsTo(Subject::class);
+    }
 }
