b985d050cced26dd113d7db4a83e8f0d24d89fb5
PenyiDavid <penyaskodavid@gmail.com>
2024-10-24 15:50:21 +0200

    New question

--- elearningOra/app/Http/Controllers/QuestionController.php
+++ elearningOra/app/Http/Controllers/QuestionController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
     public function index(){
-        return view('questions.create');
+        $subjects = Subject::all();
+        return view('questions.create', compact('subjects'));
     }
 
     public function store(Request $request){
-        
+
     }
 }
--- elearningOra/resources/views/questions/create.blade.php
+++ elearningOra/resources/views/questions/create.blade.php
@@ -7,9 +7,35 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="{{route(question.store)}}" method="POST">
+    <form action="{{route('question.store')}}" method="POST">
         @csrf
-        
+        <select name="subject_id" id="subject_id">
+            @foreach ($subjects as $subject)
+                <option value="{{$subject->id}}">{{$subject->subject_name}}</option>
+            @endforeach
+        </select><br>
+
+        <label for="question_text">Question:</label>
+        <input type="text" name="question_text" id="question_text"><br>
+
+        <label for="answer1">Answer 1</label>
+        <input type="text" name="answers[0][answer_text]" id="answers[0][answer_text]">
+        <input type="hidden" name="answers[0][is_correct]" value="0">
+        <input type="checkbox" name="answers[0][is_correct]" value="1"> Correct answer <br>    
+        <label for="answer1">Answer 2</label>
+        <input type="text" name="answers[1][answer_text]" id="answers[1][answer_text]">
+        <input type="hidden" name="answers[1][is_correct]" value="0">
+        <input type="checkbox" name="answers[1][is_correct]" value="1"> Correct answer <br>
+        <label for="answer1">Answer 3</label>
+        <input type="text" name="answers[2][answer_text]" id="answers[2][answer_text]">
+        <input type="hidden" name="answers[2][is_correct]" value="0">
+        <input type="checkbox" name="answers[2][is_correct]" value="1"> Correct answer <br>
+        <label for="answer1">Answer 4</label>
+        <input type="text" name="answers[3][answer_text]" id="answers[3][answer_text]">
+        <input type="hidden" name="answers[3][is_correct]" value="0">
+        <input type="checkbox" name="answers[3][is_correct]" value="1"> Correct answer <br>
+
+        <button type="submit">Submit</button>
     </form>
 </body>
 </html>
