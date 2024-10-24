f30e5d08061c0f89083f1f39e532422e0316c3c9
PenyiDavid <penyaskodavid@gmail.com>
2024-10-24 15:22:04 +0200

    New Question

--- elearningOra/app/Http/Controllers/QuestionController.php
+++ elearningOra/app/Http/Controllers/QuestionController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function index(){
+        return view('questions.create');
+    }
+
+    public function store(Request $request){
+        
+    }
 }
--- elearningOra/routes/web.php
+++ elearningOra/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\SubjectController;
+use App\Models\Question;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +12,9 @@ Route::get('/', function () {
 
 Route::get('/new-subject', [SubjectController::class, 'index'])->name('subject.index');
 Route::post('/new-subject', [SubjectController::class, 'store'])->name('subject.store');
+
+Route::get('/new-question', [QuestionController::class, 'index'])->name('question.index');
+Route::post('/new-question', [QuestionController::class, 'store'])->name('question.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
